b0bf20cc231bbe6c99fb7babd65d018976ef968e
hsaqlain01 <heysaqlain01@gmail.com>
2024-07-28 03:17:19 -0700

    some code is pending

--- src/screens/Leads/index.tsx
+++ src/screens/Leads/index.tsx
@@ -1,49 +1,199 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, Image, Text, View} from 'react-native';
 import React from 'react';
 import styles from '../Leads/style';
+
+const data = [
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+  {
+    id: '1',
+    name: 'John Smith',
+    amount: '7500.00',
+    position: 'Ceo SmithTech Solution',
+    company: 'Google',
+    status: 'Reopened',
+    date: '5 April, 2024',
+  },
+];
+
 const Leads = () => {
+  const renderItem = ({item}: any) => (
+    <View style={styles.mainlead}>
+      <View style={styles.leadsdata}>
+        <View style={styles.Leadsrow}>
+          <Text style={styles.leadtsxt}>{item.name}</Text>
+          <Text style={styles.leadtsxt}>{item.amount}</Text>
+        </View>
+        <View style={styles.Leadsrow}>
+          <Text style={styles.leadtsxt2}>{item.position}</Text>
+          <Text style={styles.leadtsxt3}>{item.company}</Text>
+        </View>
+        <View style={styles.topboder} />
+        <View style={styles.Leadsrow2}>
+          <View style={{flexDirection: 'row'}}>
+            <Image
+              source={require('../../assets/icons/check.png')}
+              style={styles.check}
+              resizeMode="contain"
+            />
+            <Text style={styles.leadtsxt4}>{item.status}</Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Image
+              source={require('../../assets/icons/calendar.png')}
+              style={styles.calender}
+              resizeMode="contain"
+            />
+            <Text style={styles.leadtsxt2}>{item.date}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+
   return (
     <View>
-      <Text style={styles.txt}>Lead Summery</Text>
+      <View style={{flexDirection: 'row'}}>
+        <View style={styles.boders} />
+        <Text style={styles.txt}>Lead Summery</Text>
+        <View style={{width: '55%'}}></View>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '10%',
+            marginTop: 14,
+          }}>
+          <Image
+            source={require('../../assets/icons/upload.png')}
+            style={styles.upload}
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+
       <View style={styles.row}>
         <View style={styles.rowdiv}>
-          <Text style={styles.rowtxt}>1</Text>
+          <Text style={styles.rowtxt1}>1</Text>
           <Text style={styles.rowtxt}>New Lead</Text>
         </View>
         <View style={styles.rowdiv}>
-          <Text style={styles.rowtxt}>1</Text>
+          <Text style={styles.rowtxt1}>5</Text>
           <Text style={styles.rowtxt}>Contracted</Text>
         </View>
         <View style={styles.rowdiv}>
-          <Text style={styles.rowtxt}>1</Text>
+          <Text style={styles.rowtxt1}>3</Text>
           <Text style={styles.rowtxt}>Qualified</Text>
         </View>
         <View style={styles.rowdiv}>
-          <Text style={styles.rowtxt}>1</Text>
+          <Text style={styles.rowtxt1}>6</Text>
           <Text style={styles.rowtxt}>New Lead</Text>
         </View>
       </View>
-      <View style={styles.leadtsxt}>
-        <Text>Leads</Text>
-        <Text>Filter</Text>
-      </View>
-      <View style={styles.mainlead}>
-        <View style={styles.leadsdata}>
-          <View style={styles.Leadsrow}>
-            <Text style={styles.leadtsxt}>John Smith</Text>
-            <Text style={styles.leadtsxt}>7500.00</Text>
-          </View>
-          <View style={styles.Leadsrow}>
-            <Text style={styles.leadtsxt}>Ceo SmithTech Solution</Text>
-            <Text style={styles.leadtsxt}>Google</Text>
-          </View>
-          <View style={styles.Leadsrow2}>
-            <Text style={styles.leadtsxt}>Reopened</Text>
-            <Text style={styles.leadtsxt}>5 April, 2024</Text>
-          </View>
+      <View style={styles.leadtsxt1}>
+        <Text style={styles.leadrowtxt}>Leads</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            source={require('../../assets/icons/filter.png')}
+            style={styles.filter}
+            resizeMode="contain"
+          />
+          <Text style={styles.leadrowtxt}>Filter</Text>
         </View>
+      </View>
 
-        <View></View>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      <View>
+        <Image
+          source={require('../../assets/icons/plus.png')}
+          style={styles.filter}
+          resizeMode="contain"
+        />
       </View>
     </View>
   );
--- src/screens/Leads/style.ts
+++ src/screens/Leads/style.ts
@@ -1,13 +1,20 @@
 import {StyleSheet} from 'react-native';
-import Leads from '.';
+import {THEME} from '../../constants/theme';
 
 const styles = StyleSheet.create({
   txt: {
-    marginLeft: 20,
-    borderLeftWidth: 2,
+    marginLeft: 10,
     marginTop: 20,
     borderLeftColor: '#87CEEB',
     height: 18,
+    fontFamily: 'Poppins-SemiBold',
+  },
+  boders: {
+    borderWidth: 3,
+    marginLeft: 13,
+    height: 16,
+    marginTop: 20,
+    borderColor: '#87CEEB',
   },
   row: {
     flexDirection: 'row',
@@ -17,24 +24,106 @@ const styles = StyleSheet.create({
   rowdiv: {
     width: 80,
     height: 60,
-    borderWidth: 1,
     borderRadius: 10,
     marginLeft: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    backgroundColor: THEME.WHITE,
+    elevation: 5,
   },
   rowtxt: {
     marginTop: 5,
+    fontFamily: 'Poppins-Bold',
+    alignSelf: 'center',
+    fontSize: 12,
+  },
+  rowtxt1: {
+    marginTop: 5,
     fontWeight: 'bold',
-    marginLeft: 10,
+    marginLeft: 9,
+
+    fontFamily: 'Poppins-Bold',
+    color: '#87CEEB',
   },
   leadtsxt: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    fontWeight: '900',
+    fontSize: 15,
+    fontFamily: 'Poppins-SemiBold',
+  },
+  leadtsxt4: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+    fontSize: 15,
+    color: 'purple',
+    fontFamily: 'Poppins-SemiBold',
+  },
+  check: {
+    width: 17,
+    height: 17,
+    marginRight: 5,
+    tintColor: 'purple',
+  },
+  calender: {
+    width: 17,
+    height: 17,
+    marginRight: 5,
+    tintColor: '#87CEEB',
+  },
+  upload: {
+    width: 17,
+    height: 17,
+  },
+  filter: {
+    width: 13,
+    height: 13,
+    marginRight: 3,
+    marginTop: 2,
+  },
+  leadtsxt3: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    fontWeight: '900',
+    fontSize: 13,
+  },
+  leadtsxt1: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    fontWeight: '900',
+    fontSize: 15,
+    padding: 13,
+  },
+  leadrowtxt: {
+    fontWeight: '900',
+    fontSize: 15,
+  },
+
+  leadtsxt2: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    color: '#D3D3D3',
   },
   leadsdata: {
     borderLeftWidth: 5,
     borderLeftColor: 'purple',
     borderRadius: 5,
-    marginTop: 15,
+    padding: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    backgroundColor: THEME.WHITE,
+    elevation: 5,
   },
   Leadsrow: {
     flexDirection: 'row',
@@ -43,11 +132,16 @@ const styles = StyleSheet.create({
   Leadsrow2: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 2,
-    borderTopWidth: 1,
+    marginTop: 9,
   },
   mainlead: {
     padding: 10,
+  },
+  topboder: {
+    borderWidth: 1,
+    width: '100%',
+    alignSelf: 'center',
+    borderColor: '#D3D3D3',
   },
 });
 export default styles;
